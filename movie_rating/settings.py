f9d219dabba1e76fc0f97065a8763251a3f005a7
Grant Chiu <gcalgotrading@gmail.com>
2023-08-07 21:22:07 -0700

    add whitenoise

--- movie_rating/settings.py
+++ movie_rating/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'movie_rating.urls'
