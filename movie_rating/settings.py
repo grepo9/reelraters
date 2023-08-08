090b2610bc1d64a0b1867e347da6b8ea8fb1f750
Grant Chiu <gcalgotrading@gmail.com>
2023-08-07 21:10:38 -0700

    install gunicorn

--- movie_rating/settings.py
+++ movie_rating/settings.py
@@ -131,6 +131,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+MEDIA_URL = "/img/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'movies/static/movies/img')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
