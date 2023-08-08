72a049be29e091a4bc2ab70634331238c49e5f37
Grant Chiu <gcalgotrading@gmail.com>
2023-08-07 20:48:06 -0700

    allow host

--- movie_rating/settings.py
+++ movie_rating/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d#&rnd_#@)7829hia+7_-i@mn$-mq%d69lb$#q0rtr0@^w^*dv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['reelraters-25332e1b3719.herokuapp.com']
 
 
 # Application definition
