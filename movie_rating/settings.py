22ca7f4afaf0b46e5fc5a2700e0744286ad3ee76
Grant Chiu <gcalgotrading@gmail.com>
2023-08-07 20:41:22 -0700

    re enable collectstatic

--- movie_rating/settings.py
+++ movie_rating/settings.py
@@ -135,3 +135,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
